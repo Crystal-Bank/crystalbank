4cf773355ba39f17378cc9587af95100a92ff96f
Tristan Holl <mail@tristanholl.com>
2024-10-30 20:41:40 +0100

    cleanup project base class

--- app/src/crystalbank.cr
+++ app/src/crystalbank.cr
@@ -1,16 +1,4 @@
-# Load libs
-require "pg"
-require "yaml"
-require "crystal-es"
-
-# Load config
-require "./config/load"
-
-# Load shared resources
-require "./shared/load"
-
-# Load domains
-require "./domains/accounts/load"
+require "./load"
 
 module Crystalbank
   VERSION = "0.1.0"
---
+++ app/src/load.cr
@@ -0,0 +1,13 @@
+# Load libs
+require "pg"
+require "yaml"
+require "crystal-es"
+
+# Load config
+require "./config/load"
+
+# Load shared resources
+require "./shared/load"
+
+# Load domains
+require "./domains/accounts/load"
