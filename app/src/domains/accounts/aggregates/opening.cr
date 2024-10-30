31ee976d66d065bcd8afc89bd56fa7bd97ba817d
Tristan Holl <mail@tristanholl.com>
2024-10-30 20:40:53 +0100

    add new aggregate implementation

---
+++ app/src/domains/accounts/aggregates/opening.cr
@@ -0,0 +1,23 @@
+module CrystalBank::Domains::Accounts
+  module Aggregates
+    module Concerns
+      module Opening
+        # Apply 'Accounts::Opening::Events::Accepted' to the aggregate state
+        def apply(event : Accounts::Opening::Events::Accepted)
+          @state.increase_version(event.header.aggregate_version)
+
+          body = event.body.as(Accounts::Opening::Events::Accepted::Body)
+        end
+
+        # Apply 'Accounts::Opening::Events::Requested' to the aggregate state
+        def apply(event : Accounts::Opening::Events::Requested)
+          @state.increase_version(event.header.aggregate_version)
+
+          body = event.body.as(Accounts::Opening::Events::Requested::Body)
+          @state.supported_currencies = body.currencies
+          @state.type = body.type
+        end
+      end
+    end
+  end
+end
