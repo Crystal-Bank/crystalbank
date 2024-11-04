60203018a0b132bd4a3f15844c5274b71eaf0639
Tristan Holl <mail@tristanholl.com>
2024-11-04 16:40:00 +0100

    cleanup currency validation

--- app/src/domains/accounts/commands/opening/request.cr
+++ app/src/domains/accounts/commands/opening/request.cr
@@ -6,11 +6,11 @@ module CrystalBank::Domains::Accounts
           # TODO: Replace with actor from context
           dummy_actor = UUID.new("00000000-0000-0000-0000-000000000000")
 
-          # Parse and validate provided currencies
+          # Parse and validate if provided currencies are valid and supported
           parsed_currencies = r.currencies.map { |c| CrystalBank::Types::Currency.new(c).supported! }
 
           # Parse and validate provided account type
-          parsed_type = CrystalBank::Types::AccountType.new(r.type).validate!
+          parsed_type = CrystalBank::Types::AccountType.new(r.type)
 
           # Create the account creation request event
           event = Accounts::Opening::Events::Requested.new(
