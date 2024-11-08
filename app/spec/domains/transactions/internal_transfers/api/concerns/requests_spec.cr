e40b27d01e1f7ee752e4e44128896d0a883c50f1
Tristan Holl <mail@tristanholl.com>
2024-11-08 18:21:01 +0100

    add specs skeleton for internal transfers

---
+++ app/spec/domains/transactions/internal_transfers/api/concerns/requests_spec.cr
@@ -0,0 +1,7 @@
+require "../../../../../spec_helper"
+
+describe CrystalBank::Domains::Transactions::InternalTransfers::Api::Requests do
+  pending "pending" do
+    false.should eq(true)
+  end
+end
---
+++ app/spec/domains/transactions/internal_transfers/events/initiation/requested_spec.cr
@@ -0,0 +1,7 @@
+require "../../../../../spec_helper"
+
+describe CrystalBank::Domains::Transactions::InternalTransfers::Initiation::Events::Requested do
+  pending "pending" do
+    false.should eq(true)
+  end
+end
