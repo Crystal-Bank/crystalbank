f7915c0586a0f4aff6614a151cd8e78769a104d8
Tristan Holl <mail@tristanholl.com>
2024-11-09 10:13:15 +0100

    register event handlers and add them to workflow

--- app/src/config/initializers/event_handlers.cr
+++ app/src/config/initializers/event_handlers.cr
@@ -5,4 +5,5 @@ event_handlers = ES::Config.event_handlers
 event_handlers.register(Accounts::Opening::Events::Accepted)
 event_handlers.register(Accounts::Opening::Events::Requested)
 
+event_handlers.register(Transactions::InternalTransfers::Initiation::Events::Accepted)
 event_handlers.register(Transactions::InternalTransfers::Initiation::Events::Requested)
