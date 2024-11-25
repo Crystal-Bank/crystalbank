79d751ae6e871f275fc9681fc8ed15d7decefa08
Tristan Holl <mail@tristanholl.com>
2024-11-25 21:58:33 +0100

    add queries for customers domain

---
+++ app/src/domains/customers/queries/customers.cr
@@ -0,0 +1,45 @@
+module CrystalBank::Domains::Customers
+  module Queries
+    class Customers
+      struct Customer
+        include DB::Serializable
+        include DB::Serializable::NonStrict
+        include JSON::Serializable
+
+        @[DB::Field(key: "uuid")]
+        getter id : UUID
+        getter object : String = "customer"
+
+        getter name : String
+        getter type : String
+      end
+
+      def initialize
+        @db = ES::Config.projection_database
+      end
+
+      def list(
+        cursor : UUID?,
+        limit : Int32
+      ) : Array(Customer)
+        query_param_counter = 0
+        query = [] of String
+        query_params = Array(UUID? | Int32).new
+
+        query << %(SELECT * FROM "projections"."customers" WHERE 1=1)
+
+        # Add pagination cursor to query
+        unless cursor.nil?
+          query << %(AND "uuid" >= $#{query_param_counter += 1})
+          query_params << cursor
+        end
+
+        query << %(ORDER BY "uuid" ASC)
+        query << %(LIMIT $#{query_param_counter += 1})
+        query_params << limit
+
+        @db.query_all(query.join(" "), args: query_params, as: Customer)
+      end
+    end
+  end
+end
