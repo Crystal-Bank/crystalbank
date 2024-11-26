ad739a8d180fd3b8e1fb3caca088e5d215f0b327
Tristan Holl <mail@tristanholl.com>
2024-11-26 22:43:48 +0100

    add test for customer aggregate

--- app/spec/domains/customers/aggregates/aggregate_spec.cr
+++ app/spec/domains/customers/aggregates/aggregate_spec.cr
@@ -1,7 +1,16 @@
 require "../../../spec_helper"
 
 describe CrystalBank::Domains::Customers::Aggregate do
-  pending "pending" do
-    false.should eq(true)
+  it "can be initialized" do
+    uuid = UUID.v7
+    aggr = CrystalBank::Domains::Customers::Aggregate.new(uuid)
+
+    aggr.state.aggregate_id.should eq(uuid)
+    aggr.state.aggregate_type.should eq("Customer")
+    aggr.state.aggregate_version.should eq(0)
+    
+    aggr.state.name.should eq(nil)
+    aggr.state.onboarded.should eq(false)
+    aggr.state.type.should eq(nil)
   end
 end
