7a9035e35888cc62cc9e0aa2e0be7616e3666a92
Tristan Holl <mail@tristanholl.com>
2024-12-08 18:51:53 +0100

    add tests for api keys events

--- app/spec/domains/api_keys/events/generation/requested_spec.cr
+++ app/spec/domains/api_keys/events/generation/requested_spec.cr
@@ -1,7 +1,47 @@
 require "../../../../spec_helper"
 
 describe CrystalBank::Domains::ApiKeys::Generation::Events::Requested do
-  pending "pending" do
-    false.should eq(true)
+  it "can be initialized" do
+    event = Test::ApiKey::Events::Generation::Requested.new.create
+
+    event.header.actor_id.should eq(UUID.new("00000000-0000-0000-0000-000000000000"))
+    event.header.aggregate_id.should eq(UUID.new("00000000-0000-0000-0000-000000000001"))
+    event.header.aggregate_type.should eq("ApiKey")
+    event.header.aggregate_version.should eq(1)
+    event.header.command_handler.should eq("test")
+    event.header.event_handle.should eq("api_key.generation.requested")
+    event.body.comment.should eq("test comment")
+  end
+
+  it "can be initialized from json" do
+    object = Test::ApiKey::Events::Generation::Requested.new
+    event = object.create
+    h = event.header
+
+    json_body = JSON.parse(object.json_string)
+
+    e = ApiKeys::Generation::Events::Requested.new(h, json_body)
+    b = e.body.as(ApiKeys::Generation::Events::Requested::Body)
+
+    b.comment.should eq("test comment")
+  end
+
+  it "can serialize to json" do
+    object = Test::ApiKey::Events::Generation::Requested.new
+    event = object.create
+
+    b = event.body.as(ApiKeys::Generation::Events::Requested::Body)
+    b.to_json.should eq(object.json_string)
+  end
+
+  it "can serialize and deserialize from json" do
+    event = Test::ApiKey::Events::Generation::Requested.new.create
+    h = event.header
+
+    b = event.body.as(ApiKeys::Generation::Events::Requested::Body)
+    json_body = b.to_json
+
+    e = ApiKeys::Generation::Events::Requested.new(h, JSON.parse(json_body))
+    json_body.should eq(e.body.to_json)
   end
 end
---
+++ app/spec/factories/events/api_key_factories.cr
@@ -0,0 +1,111 @@
+module Test::ApiKey::Events
+  module Generation
+    class Accepted
+      def create(aggr_id = UUID.new("00000000-0000-0000-0000-000000000001")) : ApiKeys::Generation::Events::Accepted
+        actor_id = UUID.new("00000000-0000-0000-0000-000000000000")
+        aggregate_id = aggr_id
+        aggregate_version = 2
+        command_handler = "test"
+        comment = "test comment"
+
+        ApiKeys::Generation::Events::Accepted.new(
+          actor_id: actor_id,
+          aggregate_id: aggregate_id,
+          aggregate_version: aggregate_version,
+          command_handler: command_handler,
+          comment: comment
+        )
+      end
+
+      def json_string : String
+        {
+          "comment": "test comment",
+        }.to_json
+      end
+    end
+
+    class Requested
+      def create(aggr_id = UUID.new("00000000-0000-0000-0000-000000000001")) : ApiKeys::Generation::Events::Requested
+        actor_id = UUID.new("00000000-0000-0000-0000-000000000000")
+        aggregate_id = aggr_id
+        api_secret = "api_secret"
+        command_handler = "test"
+        comment = "test comment"
+        name = "test name"
+        user_id = UUID.new("00000000-0000-0000-0000-000000000000")
+
+        ApiKeys::Generation::Events::Requested.new(
+          actor_id: actor_id,
+          aggregate_id: aggregate_id,
+          api_secret: api_secret,
+          command_handler: command_handler,
+          comment: comment,
+          name: name,
+          user_id: user_id
+        )
+      end
+
+      def json_string : String
+        {
+          "comment":    "test comment",
+          "name":       "test name",
+          "user_id":    "00000000-0000-0000-0000-000000000000",
+          "api_secret": "api_secret",
+        }.to_json
+      end
+    end
+  end
+
+  module Revocation
+    class Accepted
+      def create(aggr_id = UUID.new("00000000-0000-0000-0000-000000000001")) : ApiKeys::Revocation::Events::Accepted
+        actor_id = UUID.new("00000000-0000-0000-0000-000000000000")
+        aggregate_id = aggr_id
+        aggregate_version = 2
+        command_handler = "test"
+        comment = "test comment"
+
+        ApiKeys::Revocation::Events::Accepted.new(
+          actor_id: actor_id,
+          aggregate_id: aggregate_id,
+          aggregate_version: aggregate_version,
+          command_handler: command_handler,
+          comment: comment
+        )
+      end
+
+      def json_string : String
+        {
+          "comment": "test comment",
+        }.to_json
+      end
+    end
+
+    class Requested
+      def create(aggr_id = UUID.new("00000000-0000-0000-0000-000000000001")) : ApiKeys::Revocation::Events::Requested
+        actor_id = UUID.new("00000000-0000-0000-0000-000000000000")
+        aggregate_id = aggr_id
+        aggregate_version = 1
+        reason = "revocation reason"
+        command_handler = "test"
+        comment = "test comment"
+
+        ApiKeys::Revocation::Events::Requested.new(
+          actor_id: actor_id,
+          aggregate_id: aggregate_id,
+          aggregate_version: aggregate_version,
+          command_handler: command_handler,
+          comment: comment,
+          reason: reason
+        )
+      end
+
+      def json_string : String
+        {
+          "comment": "test comment",
+          "reason":  "revocation reason",
+        }.to_json
+      end
+    end
+  end
+end
