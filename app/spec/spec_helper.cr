3f007e09a2d54aab9c2e97cd2fccec8f7bbc354c
Tristan Holl <mail@tristanholl.com>
2024-12-08 19:11:10 +0100

    add api keys event factories

--- app/spec/spec_helper.cr
+++ app/spec/spec_helper.cr
@@ -1,6 +1,7 @@
 require "spec"
 require "../src/crystalbank"
 require "./factories/events/account_factories"
+require "./factories/events/api_key_factories"
 require "./factories/events/customer_factories"
 require "./factories/events/transaction_factories"
 require "./factories/events/user_factories"
