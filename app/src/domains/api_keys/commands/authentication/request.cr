70f3c361d8fc1f665b7efcbad1b5101813804543
Tristan Holl <mail@tristanholl.com>
2024-12-13 17:38:10 +0100

    update authentication request command (WIP)

--- app/src/domains/api_keys/commands/authentication/request.cr
+++ app/src/domains/api_keys/commands/authentication/request.cr
@@ -5,20 +5,37 @@ module CrystalBank::Domains::ApiKeys
         def call(client_id : String, client_secret : String) : String
           @aggregate_id = UUID.new(client_id)
 
-          # # Fetch API key
-          # api_key = ApiKeys::Repositories::ApiKeys.new.fetch(@aggregate_id)
+          # Fetch API key
+          aggregate = ApiKeys::Aggregate.new(UUID.new(client_id))
+          aggregate.hydrate
 
-          # # Fail if api key is inactive
-          # raise CrystalBank::Exception::Authentication.new("Invalid login credentials", HTTP::Status::UNAUTHORIZED) unless api_key.active
+          api_key_active = aggregate.state.active
+          user_id = aggregate.state.user_id 
+          encrypted_secret = aggregate.state.encrypted_secret
 
-          # # Extract attributes to local variables
-          # api_secret = Crypto::Bcrypt::Password.new(api_key.encrypted_secret)
+          # Check integrity of aggregate state
+          raise ES::Exception::InvalidState.new("Cannot verify api credentials") if encrypted_secret.nil?
+          raise ES::Exception::InvalidState.new("Api key is missing user releation") if user_id.nil?
 
-          # # Check if the secret is valid
-          # raise CrystalBank::Exception::Authentication.new("Invalid login credentials", HTTP::Status::UNAUTHORIZED) unless api_secret.verify(client_secret)
+          # Fail if api key is inactive
+          raise CrystalBank::Exception::Authentication.new("Invalid login credentials", HTTP::Status::UNAUTHORIZED) unless api_key_active
 
-          # # Fetch user
-          # user = ApiKeys::Repositories::Users.new.fetch(api_key.user_id)
+          # Extract attributes to local variables
+          api_secret = Crypto::Bcrypt::Password.new(encrypted_secret)
+
+          # Check if the secret is valid
+          raise CrystalBank::Exception::Authentication.new("Invalid login credentials", HTTP::Status::UNAUTHORIZED) unless api_secret.verify(client_secret)
+
+          # Fetch user
+          user = ApiKeys::Repositories::Users.new.fetch!(user_id)
+
+          # TODO: Extract user roles
+          
+          # TODO: Create JWT and return it
+          # payload = CrystalBank::Objects::JWTPayload.new(roles: user_roles, user_id: @aggregate_id)
+          # JWT.encode(payload, CrystalBank::Env.jwt_private_key, JWT::Algorithm::ES256)
+
+          CrystalBank.print_verbose("User", user)
 
           # # Fetch roles the user is assigned to
           # user_roles = user.roles
