18d00db6d860a1dd005ad707b6dde8051eb5c844
Tristan Holl <mail@tristanholl.com>
2025-02-20 16:24:28 +0100

    small bugfix for authorization

--- app/src/shared/api/authorization.cr
+++ app/src/shared/api/authorization.cr
@@ -21,13 +21,13 @@ module CrystalBank
 
         # Fetch available scopes
         available_scopes = CrystalBank::Services::AccessControl.new(roles: jwt.data.roles).available_scopes(parsed_permissions)
-        raise CrystalBank::Exception::Authorization.new("No permission to perform this action '#{permission}' on scope '#{scope_id}'") unless available_scopes.includes?(scope_id)
+        raise CrystalBank::Exception::Authorization.new("No permission to perform action '#{permission}' on scope '#{scope_id}'") if scope_id && !available_scopes.includes?(scope_id)
 
         # Create a session object
         @context = CrystalBank::Api::Context.new(jwt.data.user, jwt.data.roles, parsed_permissions, scope_id, available_scopes)
 
         return true unless available_scopes.empty?
-        raise CrystalBank::Exception::Authorization.new("No permission to perform this action '#{permission}'")
+        raise CrystalBank::Exception::Authorization.new("No permission to perform action '#{permission}'")
       end
 
       private def parse_jwt(token : String) : CrystalBank::Api::JWT
