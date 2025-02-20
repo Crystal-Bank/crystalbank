d13ae94764ea46c1bf396c0259058c6818679e39
Tristan Holl <mail@tristanholl.com>
2025-02-20 14:45:48 +0100

    update seed file

--- app/src/seed.cr
+++ app/src/seed.cr
@@ -7,6 +7,9 @@ dummy_uuid = UUID.new("00000000-0000-0000-0000-000000000000")
 api_secret = "secret"
 api_secret_encrypted = Crypto::Bcrypt::Password.create(api_secret, cost: 10).to_s
 
+# +++++++++++++++++++++++
+# User seed {START}
+# +++++++++++++++++++++++
 # Create User
 event = Users::Onboarding::Events::Requested.new(
   actor_id: dummy_uuid,
@@ -20,7 +23,13 @@ event_store.append(event)
 
 # Return the aggregate ID of the newly created user aggregate
 user_id = UUID.new(event.header.aggregate_id.to_s)
+# +++++++++++++++++++++++
+# User seed {END}
+# +++++++++++++++++++++++
 
+# +++++++++++++++++++++++
+# API key seed {START}
+# +++++++++++++++++++++++
 # Create API key
 event = ApiKeys::Generation::Events::Requested.new(
   actor_id: dummy_uuid,
@@ -29,9 +38,30 @@ event = ApiKeys::Generation::Events::Requested.new(
   name: "admin api key",
   user_id: user_id
 )
-
 # Append event to event store
 event_store.append(event)
+# +++++++++++++++++++++++
+# API key seed {END}
+# +++++++++++++++++++++++
+
+# +++++++++++++++++++++++
+# Role seed {START}
+# +++++++++++++++++++++++
+
+# Create Role
+event = Roles::Creation::Events::Requested.new(
+  actor_id: dummy_uuid,
+  command_handler: "seed",
+  name: "Admin Role",
+  permissions: CrystalBank::Permissions.values,
+  scopes: [] of UUID # TODO: Add top level admin scope
+)
+# Append event to event store
+event_store.append(event)
+role_id = UUID.new(event.header.aggregate_id.to_s)
+# +++++++++++++++++++++++
+# Role seed {END}
+# +++++++++++++++++++++++
 
 # Return the aggregate ID of the newly created user aggregate
 output = [
@@ -39,3 +69,9 @@ output = [
   "client_secret: 'secret'",
 ]
 CrystalBank.print_verbose("Seed credentials", output.join("\n"))
+
+entities = [
+  "Admin Role: '#{role_id}'",
+  "Root Scope: 'TBD'",
+]
+CrystalBank.print_verbose("Created entities", output.join("\n"))
