8a22e3e7dc65b610f25c607164abc87ea3666de6
Tristan Holl <mail@tristanholl.com>
2025-02-20 11:49:48 +0100

    check parent scope id

--- app/src/domains/scopes/commands/creation/request.cr
+++ app/src/domains/scopes/commands/creation/request.cr
@@ -5,15 +5,20 @@ module CrystalBank::Domains::Scopes
         def call(r : Scopes::Api::Requests::CreationRequest) : UUID
           # TODO: Replace with actor from context
           dummy_actor = UUID.new("00000000-0000-0000-0000-000000000000")
+          parent_scope_id = r.parent_scope_id
 
-          # Check if parent scope is valid
-          #
+          # Check if provided parent scope can be found
+          unless parent_scope_id.nil?
+            aggregate = Scopes::Aggregate.new(parent_scope_id)
+            aggregate.hydrate
+          end
+
           # Create the scope creation request event
           event = Scopes::Creation::Events::Requested.new(
             actor_id: dummy_actor,
             command_handler: self.class.to_s,
             name: r.name,
-            parent_scope_id: r.parent_scope_id
+            parent_scope_id: parent_scope_id
           )
 
           # Append event to event store
