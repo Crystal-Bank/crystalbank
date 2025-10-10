bc4fe47c03b60cc10a3591021146dcd9f600db4c
Tristan Holl <mail@tristanholl.com>
2025-10-11 00:26:34 +0200

    check openapi spec

--- app/spec/crystalbank_spec.cr
+++ app/spec/crystalbank_spec.cr
@@ -1,9 +1,16 @@
 require "./spec_helper"
 
 describe CrystalBank do
-  # TODO: Write tests
+  # Test OpenAPI generator
+  it "extracts route descriptions" do
+    result = ActionController::OpenAPI.extract_route_descriptions
+    (result.size > 0).should be_true
+  end
 
-  it "works" do
-    true.should eq(true)
+  it "generates openapi docs" do
+    result = ActionController::OpenAPI.generate_open_api_docs("title", "version", description: "desc")
+    result[:openapi].should eq "3.0.3"
+    result[:paths].size.should eq 17
+    result[:info][:description].should eq "desc"
   end
 end
