ea864ebbdb4de48611a7ea6ecb3735382b9eb042
Tristan Holl <mail@tristanholl.com>
2026-03-05 17:43:58 +0100

    cleanup transaction event

--- app/src/domains/transactions/internal_transfers/events/initiation/requested.cr
+++ app/src/domains/transactions/internal_transfers/events/initiation/requested.cr
@@ -12,33 +12,6 @@ module CrystalBank::Domains::Transactions::InternalTransfers
           attribute remittance_information, String
           attribute scope_id, UUID
         end
-
-        @@type = "Transaction"
-        @@handle = "transactions.internal_transfer.initiation.requested"
-
-        # Data Object for the body of the event
-        struct Body < ES::Event::Body
-          getter currency : CrystalBank::Types::Currencies::Supported
-          getter amount : Int64
-          getter creditor_account_id : UUID
-          getter debtor_account_id : UUID
-          getter remittance_information : String
-          getter scope_id : UUID
-
-          def initialize(
-            @amount : Int64,
-            @comment : String,
-            @creditor_account_id : UUID,
-            @currency : CrystalBank::Types::Currencies::Supported,
-            @debtor_account_id : UUID,
-            @remittance_information : String,
-            @scope_id : UUID,
-          ); end
-        end
-
-        def initialize(@header : ES::Event::Header, body : JSON::Any)
-          @body = Body.from_json(body.to_json)
-        end
       end
     end
   end
