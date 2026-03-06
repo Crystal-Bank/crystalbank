982baa634d76c2c5acb381d97b548977fcf97cbf
Tristan Holl <tristanholl@users.noreply.github.com>
2026-03-07 00:00:26 +0100

    update and error fix (#43)
    * update and error fix
    
    * beautify json

--- app/src/shared/api/error_handler.cr
+++ app/src/shared/api/error_handler.cr
@@ -1,20 +1,20 @@
-require "action-controller/server"
+# require "action-controller/server"
 
-class ApiErrorHandler
-  include HTTP::Handler
-  include CrystalBank::Api::Responses
+# class ApiErrorHandler
+#   include HTTP::Handler
+#   include CrystalBank::Api::Responses
 
-  def call(context)
-    begin
-      call_next(context)
-    rescue ex : ::Exception
-      context.response.status_code = 500
-      context.response.content_type = "application/json"
-      context.response.print(
-        ErrorResponse.new(UUID.v7, ex.message.to_s, Time.utc, ex.class.to_s).to_json
-      )
-    end
-  end
-end
+#   def call(context)
+#     begin
+#       call_next(context)
+#     rescue ex : ::Exception
+#       context.response.status_code = 500
+#       context.response.content_type = "application/json"
+#       context.response.print(
+#         ErrorResponse.new(UUID.v7, ex.message.to_s, Time.utc, ex.class.to_s).to_json
+#       )
+#     end
+#   end
+# end
 
-ActionController::Server.before ApiErrorHandler.new
+# ActionController::Server.before ApiErrorHandler.new
