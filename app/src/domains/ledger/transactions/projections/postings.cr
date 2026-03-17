dd9f8e500e9cc2ef0e9b0ef260fedd502206dde9
Tristan Holl <mail@tristanholl.com>
2026-03-17 19:57:49 +0100

    bugfix

--- app/src/domains/ledger/transactions/projections/postings.cr
+++ app/src/domains/ledger/transactions/projections/postings.cr
@@ -85,7 +85,7 @@ module CrystalBank::Domains::Ledger::Transactions
                   external_ref,
                   channel
                 )
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
             ),
               entry.id,
               aggregate_id,
