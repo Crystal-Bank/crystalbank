2a58915b863deac302b530a1753e2b7dc46e0a57
Tristan Holl <mail@tristanholl.com>
2026-03-27 21:17:50 +0100

    cleanup

--- app/src/config/types/account_blocks.cr
+++ app/src/config/types/account_blocks.cr
@@ -1,19 +1,19 @@
 module CrystalBank::Types::Accounts
   enum BlockType
-    ComplianceBlock
-    OperationsBlock
-    GenericDebitBlock
-    GenericCreditBlock
-    GenericFullBlock
+    COMPLIANCE_BLOCK
+    OPERATIONS_BLOCK
+    GENERIC_DEBIT_BLOCK
+    GENERIC_CREDIT_BLOCK
+    GENERIC_FULL_BLOCK
 
     def effect : BlockEffect
       case self
-      when ComplianceBlock, GenericFullBlock
-        BlockEffect::Full
-      when OperationsBlock, GenericDebitBlock
-        BlockEffect::Debit
-      when GenericCreditBlock
-        BlockEffect::Credit
+      when COMPLIANCE_BLOCK, GENERIC_FULL_BLOCK
+        BlockEffect::FULL
+      when OPERATIONS_BLOCK, GENERIC_DEBIT_BLOCK
+        BlockEffect::DEBIT
+      when GENERIC_CREDIT_BLOCK
+        BlockEffect::CREDIT
       else
         raise "Unhandled block type: #{self}"
       end
@@ -25,28 +25,28 @@ module CrystalBank::Types::Accounts
   end
 
   enum BlockEffect
-    Full
-    Debit
-    Credit
+    FULL
+    DEBIT
+    CREDIT
   end
 
   enum EffectiveBlock
-    FullBlock
-    DebitBlock
-    CreditBlock
-    None
+    FULL_BLOCK
+    DEBIT_BLOCK
+    CREDIT_BLOCK
+    NONE
 
     def self.evaluate(active_blocks : Array(BlockType)) : EffectiveBlock
-      return None if active_blocks.empty?
+      return NONE if active_blocks.empty?
 
       effects = active_blocks.map(&.effect).uniq
 
-      return FullBlock if effects.includes?(BlockEffect::Full)
-      return FullBlock if effects.includes?(BlockEffect::Debit) && effects.includes?(BlockEffect::Credit)
-      return DebitBlock if effects.includes?(BlockEffect::Debit)
-      return CreditBlock if effects.includes?(BlockEffect::Credit)
+      return FULL_BLOCK if effects.includes?(BlockEffect::FULL)
+      return FULL_BLOCK if effects.includes?(BlockEffect::DEBIT) && effects.includes?(BlockEffect::CREDIT)
+      return DEBIT_BLOCK if effects.includes?(BlockEffect::DEBIT)
+      return CREDIT_BLOCK if effects.includes?(BlockEffect::CREDIT)
 
-      None
+      NONE
     end
 
     def to_s
