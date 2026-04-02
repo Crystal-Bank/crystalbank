9049de77ff0767bcb51c08569f8aeb8f4a180174
Tristan Holl <mail@tristanholl.com>
2026-04-02 10:29:33 +0200

    change tool implementation

--- app/tools/generate_jwt_keys.cr
+++ app/tools/generate_jwt_keys.cr
@@ -1,17 +1,6 @@
-require "openssl"
+require "openssl_ext"
 
-ec_key = OpenSSL::EC::Key.new("prime256v1")
+key = OpenSSL::PKey::EC.new(256)
 
-private_key = ec_key.to_pem
-public_key = ec_key.public_key.to_pem
-
-puts "=== JWT ES256 Key Pair ==="
-puts ""
-puts "--- Private Key (JWT_PRIVATE_KEY) ---"
-puts private_key
-puts "--- Public Key (JWT_PUBLIC_KEY) ---"
-puts public_key
-puts ""
-puts "Set environment variables:"
-puts %(JWT_PRIVATE_KEY="#{private_key.gsub("\n", "\\n")}")
-puts %(JWT_PUBLIC_KEY="#{public_key.gsub("\n", "\\n")}")
+puts key.to_pem
+puts key.public_key.to_pem
