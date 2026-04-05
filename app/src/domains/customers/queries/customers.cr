6ffc496029697f3d3ff9c3518c6aa218adc29136
Tristan Holl <mail@tristanholl.com>
2026-04-05 08:57:04 +0200

    optimize query

--- app/src/domains/customers/queries/customers.cr
+++ app/src/domains/customers/queries/customers.cr
@@ -20,24 +20,36 @@ module CrystalBank::Domains::Customers
         @db = ES::Config.projection_database
       end
 
-      def find_all(uuids : Array(UUID), scope_id : UUID? = nil, status : String? = nil) : Array(Customer)
+      def find_all(
+        context : CrystalBank::Api::Context,
+        uuids : Array(UUID), 
+        scope_id : UUID? = nil, 
+        status : String? = nil
+      ) : Array(Customer)
         return Array(Customer).new if uuids.empty?
+        query_param_counter = 0
+        query = [] of String
+        query_params = Array(Array(UUID) | UUID? | String?).new
+        
+        query << %(SELECT * FROM "projections"."customers" WHERE 1=1)
+
+        query_params << uuids
         conditions = [%(\"uuid\" = ANY($1))]
-        params = Array(Array(UUID) | UUID? | String?).new
-        params << uuids
+
+        # Add scope query
+        query << %(AND "scope_id" = ANY($#{query_param_counter += 1}::uuid[]))
+        query_params << context.available_scopes
+
         if scope_id
-          conditions << %(\"scope_id\" = $#{params.size + 1})
-          params << scope_id
+          query << %(AND "scope_id" = $#{query_param_counter += 1})
+          query_params << scope_id
         end
         if status
-          conditions << %(\"status\" = $#{params.size + 1})
-          params << status
+          query << %(AND "status" = $#{query_param_counter += 1})
+          query_params << status
         end
-        @db.query_all(
-          %(SELECT * FROM "projections"."customers" WHERE #{conditions.join(" AND ")}),
-          args: params,
-          as: Customer
-        )
+
+        @db.query_all(query.join(" "), args: query_params, as: Customer)
       end
 
       def list(
