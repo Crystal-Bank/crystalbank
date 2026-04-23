76b1493a69284c61257c8c502bd9a73c73862d4b
Tristan Holl <tristanholl@users.noreply.github.com>
2026-04-23 09:16:29 +0200

    Update frontend branding logo and add favicon links (#124)
    * Update frontend logos and add favicon assets
    
    * embed the necessary images into the compiled artifact

--- app/src/server/frontend.cr
+++ app/src/server/frontend.cr
@@ -7,9 +7,14 @@ module CrystalBank
     class Frontend < ::AC::Base
       base "/"
 
-      INDEX_HTML = {{ read_file("#{__DIR__}/../../public/index.html") }}
-      APP_JS     = {{ read_file("#{__DIR__}/../../public/app.js") }}
-      APP_CSS    = {{ read_file("#{__DIR__}/../../public/style.css") }}
+      INDEX_HTML        = {{ read_file("#{__DIR__}/../../public/index.html") }}
+      APP_JS            = {{ read_file("#{__DIR__}/../../public/app.js") }}
+      APP_CSS           = {{ read_file("#{__DIR__}/../../public/style.css") }}
+      APP_LOGO_SVG      = {{ read_file("#{__DIR__}/../../public/images/logo/crystalbank-horizontal-dark.svg") }}
+      APP_FACICON_ICO   = {{ read_file("#{__DIR__}/../../public/images/logo/crystalbank-favicon.ico") }}
+      APP_FAVICON_SVG   = {{ read_file("#{__DIR__}/../../public/images/logo/crystalbank-favicon.svg") }}
+      APP_FAVICON32_png = {{ read_file("#{__DIR__}/../../public/images/logo/crystalbank-favicon-32x32.png") }}
+      APP_FAVICON16_png = {{ read_file("#{__DIR__}/../../public/images/logo/crystalbank-favicon-16x16.png") }}
 
       @[AC::Route::Filter(:before_action)]
       def check_dashboard_domain
@@ -35,6 +40,36 @@ module CrystalBank
       get "/style.css" do
         response.content_type = "text/css; charset=utf-8"
         response.print APP_CSS
+        response.close
+      end
+
+      get "/images/logo/crystalbank-favicon.ico" do
+        response.content_type = "image/x-icon"
+        response.print APP_FACICON_ICO
+        response.close
+      end
+
+      get "/images/logo/crystalbank-favicon.svg" do
+        response.content_type = "image/svg+xml; charset=utf-8"
+        response.print APP_FAVICON_SVG
+        response.close
+      end
+
+      get "/images/logo/crystalbank-favicon-32x32.png" do
+        response.content_type = "image/png"
+        response.print APP_FAVICON32_png
+        response.close
+      end
+
+      get "/images/logo/crystalbank-favicon-16x16.png" do
+        response.content_type = "image/png"
+        response.print APP_FAVICON16_png
+        response.close
+      end
+
+      get "/images/logo/crystalbank-horizontal-dark.svg" do
+        response.content_type = "image/svg+xml; charset=utf-8"
+        response.print APP_LOGO_SVG
         response.close
       end
     end
